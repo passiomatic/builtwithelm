7349f17d194e399f279751b62c0f6ab797601940
Dwayne Crooks <me@dwaynecrooks.com>
2022-03-13 07:19:40 -0400

    Add a deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clean
+
+rm public/app.js
+
+# Build
+
+./scripts/build-prod.sh src/Main.elm
+
+# Deploy
+
+path="deploy"
+
+git worktree add $path netlify
+
+rm -rf $path/*
+cp -r public/* $path
+git -C $path add .
+if git -C $path commit -m "Site updated to $(git log -n 1 --format='%h' master)"; then
+  git -C $path push -u origin HEAD
+fi
+
+git worktree remove --force $path
+echo "Success!"
