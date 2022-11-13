944031f25183bb7f0c571941476d160396c27492
Dwayne Crooks <me@dwaynecrooks.com>
2022-11-13 10:23:28 -0400

    Refactor the developer environment

--- shell.nix
+++ shell.nix
@@ -1,21 +1,20 @@
-{
-  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/88eab1e431cabd0ed621428d8b40d425a07af39f.tar.gz") {}
-}:
-
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d8f2c4d846a2e65ad3f5a5e842b672f0b81588a2.tar.gz") {};
+in
 pkgs.mkShell {
-  buildInputs = [
+  packages = [
     pkgs.caddy
     pkgs.elmPackages.elm
     pkgs.elmPackages.elm-format
-    pkgs.nodejs-16_x
-    pkgs.nodePackages.terser
+    pkgs.nodejs-18_x
   ];
 
   shellHook =
     ''
     export project="$PWD"
     export build="$project/.build"
-
     export PATH="$project/bin:$PATH"
+
+    npm install --loglevel error >/dev/null
     '';
 }
